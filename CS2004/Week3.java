5ec14f5f78a84a21e2259d589be9969b016bbedb
Rakibul Bhuiyan <80211974+RakibulBh@users.noreply.github.com>
2024-10-14 12:04:53 +0100

    Compaering algorithms

--- CS2004/Week3.java
+++ CS2004/Week3.java
@@ -17,4 +17,31 @@ public class Main {
 
         return ans;
     }
+
+    
+    // O(N^2) Run time
+    public static double[] alg1(double[] X) {
+        double[] A = new double[X.length];
+        for (int i = 0; i<X.length; i++) {
+            double s = X[0];
+            for (int j = 0; j < A.length; j++) {
+                if (j <= i){
+                    s = s + X[j];
+                }
+            }
+            A[i] = s / (i + 1);
+        }
+        return A;
+    }
+
+    // O(N) runtime
+    public static double[] alg2(double[] X) {
+        double[] A = new double[X.length];
+        double s = 0;
+        for (int i = 0; i<X.length; i++) {
+            s = s + X[i];
+            A[i] = s / (i + 1);
+        }
+        return A;
+    }
 }
